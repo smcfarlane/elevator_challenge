ae7763c3c7bb3a92d05b5ad7c790a0736d6e54da
Steven McFarlane <stevemac72@gmail.com>
2018-07-30 20:58:40 -0700

    work on floor moving logic

--- building.js
+++ building.js
@@ -9,4 +9,16 @@ class Building {
       this.elevators.push(new Elevator())
     }
   }
+
+  requestElevator(atFloor, toFloor) {
+    if (atFloor && (atFloor > 1 || atFloor < this.topFloor)) {
+      console.error('you cannot be on that floor it does not exist')
+      return
+    }
+    if (toFloor && (toFloor > 1 || toFloor < this.topFloor)) {
+      console.error('The floor you are requesting does not exist')
+      return
+    }
+    var elevator = this._fetchNearestElevator(atFloor, toFloor)
+  }
 }
--- elevator.js
+++ elevator.js
@@ -14,12 +14,41 @@ class Elevator {
   }
 
   moveTo(floorNumber) {
-
+    this.state = 'moving'
+    this.destinationFloor = floorNumber
+    if (this.destinationFloor === this.currentFloor) {
+      this.state = 'stopped'
+      return this
+    } else if (this.destinationFloor > this.currentFloor) {
+      return this._moveUpToDestination()
+    } else if (this.destinationFloor < this.currentFloor) {
+      return this._moveDownToDestination()
+    }
   }
 
-  moveOneFloorUp() {
+  async _moveOneFloorUp() {
     this.state = 'moving'
+    await sleep(2000)
     this.currentFloor += 1
     console.log(`Elevator Number ${this.number} is now at floor ${this.currentFloor}.`)
+  }
+
+  async _moveUpToDestination() {
+    while(this.destinationFloor > this.currentFloor) {
+      await this._moveOneFloorUp()
+    }
+  }
+
+  async _moveOneFloorDown() {
+    this.state = 'moving'
+    await sleep(2000)
+    this.currentFloor -= 1
+    console.log(`Elevator Number ${this.number} is now at floor ${this.currentFloor}.`)
+  }
+
+  async _moveDownToDestination() {
+    while(this.destinationFloor > this.currentFloor) {
+      await this._moveOneFloorDown()
+    }
   }
 }
